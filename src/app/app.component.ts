6e5eb0ea841f0a86f7e9ec62dc163261c1da6bac
Artem Chernenko <fantom96fantom96@outlook.com>
2021-07-22 16:35:04 +0300

    v1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
+import { filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-first-app';
+
+  strings : string[] = ['6667','i88i']
+
+  constructor() {
+    this.string$.pipe(
+      filter(str => !str.split('').includes('0')),
+    ).subscribe(str => this.strings.push(str))
+  }
+
+  private generateRandomStr() {
+    const characters : string = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let result = ''
+    for ( let i = 0; i < 5; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return result
+  }
+  checkPalindrome( str: string) {
+    let reversStr = str.split('').reverse().join('')
+    return reversStr === str;
+  }
+  checkStrOfDigits(str: string) {
+    return /^\d+$/.test(str)
+  }
+
+  private string$ = new Observable<string>(observer => {
+    setInterval(() => observer.next(this.generateRandomStr()), 3000)
+  });
+
 }
